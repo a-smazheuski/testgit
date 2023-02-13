c1b127003c99244bc919ac3353052f9cd01cf7a5
a-smazheuski <timmy.lepel@gmail.com>
2023-02-13 23:14:36 +0300

    new ssh test 13.02.2023

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            hello world
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur blanditiis laudantium officia repellat? Dolores ipsam magnam numquam perspiciatis, quasi temporibus!</p>
+        </div>
+    );
 }
 
 export default App;
